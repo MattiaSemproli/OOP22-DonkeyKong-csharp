980756d2ab7d2e8ab110a81b194b1697f0389bd7
Giacomo Foschi <giacomo.foschi3@studio.unibo.it>
2023-06-20 17:13:44 +0200

    Added Abstract component implementation

---
+++ Foschi/DonkeyKongGameSolution/DonkeyKongGame/UtilityCode/AbstractComponent.cs
@@ -0,0 +1,18 @@
+﻿namespace DonkeyKongGame
+{
+    /// <summary>
+    /// AbstractComponent class, manages a component and interaction with entity.
+    /// </summary>
+    public abstract class AbstractComponent : IComponent
+    {
+        //In the Java project, this field is protected because it is visible in all the package.
+        //To traduce in C#, I have to set public the field.
+        /// <summary>
+        /// The entity linked to the component.
+        /// </summary>
+        public IEntity? Entity { get; set; }
+
+        /// <inheritdoc />
+        public abstract void Update();
+    }
+}
