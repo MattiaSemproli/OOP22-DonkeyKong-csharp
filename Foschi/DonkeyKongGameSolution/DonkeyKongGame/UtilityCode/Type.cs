4875a19b2e42598bc965755778c4e6a0c3d4865a
Giacomo Foschi <giacomo.foschi3@studio.unibo.it>
2023-06-20 17:14:49 +0200

    Added utility classes

---
+++ Foschi/DonkeyKongGameSolution/DonkeyKongGame/UtilityCode/Type.cs
@@ -0,0 +1,19 @@
+﻿namespace DonkeyKongGame
+{
+    public enum Type
+    {
+        PLAYER,
+        MONKEY,
+        PRINCESS,
+        BARREL,
+        BLOCK,
+        LADDER,
+        BLOCK_LADDER_UP,
+        BLOCK_LADDER_DOWN,
+        BLOCK_LADDER_UPDOWN,
+        SHIELD,
+        HEART,
+        STAR,
+        SNOWFLAKE,
+    }
+}
